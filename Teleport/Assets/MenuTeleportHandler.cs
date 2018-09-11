8a52c31ba85ea993f015d43d1b4a8b59f3fb3fff
Lucas Rumney <lucasrumney94@gmail.com>
2018-09-11 16:55:25 -0700

    add menu-based teleportation basics

---
+++ Teleport/Assets/MenuTeleportHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTeleportHandler : MonoBehaviour {
+
+	public List<Vector3> locations = new List<Vector3>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	public void TeleportPlayerTo(int locationIndex)
+	{
+		transform.position = locations[locationIndex];
+	}
+}
